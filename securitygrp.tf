8654dc758e1d4db196658083a56c7701e2a104bf
ther1chie <52828691+ther1chie@users.noreply.github.com>
2020-12-19 21:21:44 +0530

    firewall

---
+++ securitygrp.tf
@@ -0,0 +1,41 @@
+# Creating security group or firewall
+resource  "aws_security_group" "efs_firewall" {
+  name          = "allow"
+  description = "Allow TLS inbound traffic"
+  vpc_id        = "vpc-0c0d1664"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "EFS"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "efs-sg"
+  }
+}
